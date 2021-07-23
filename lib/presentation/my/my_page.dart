6bd439a033a83586391e42c41c56f0a0d6078ae6
sekijun1 <70083776+sekijun1@users.noreply.github.com>
2021-07-23 12:09:04 +0900

    issues#2 アカウント情報を確認できるマイページ画面を作成する (#4)
    * 7/16 枠組み完成/誕生日ドラムロール未
    
    * 7/17 生年月日ドラムロール採用
    
    * 7/18 生年月日ドラムロール採用
    
    * 7/19
    
    * 7/20 車種・車検日更新済み
    
    * 7/21 Pull request コメント内容修正
    
    * 7/23 GestureDetector設定場所を訂正

--- lib/presentation/my/my_page.dart
+++ lib/presentation/my/my_page.dart
@@ -1,7 +1,6 @@
-import 'package:eastarrow_app/presentation/my/my_member_info/email_change_page/email_change_page.dart';
-import 'package:eastarrow_app/presentation/my/my_member_info/password_change_page/password_change_page.dart';
+import 'package:eastarrow_app/presentation/common/dialog.dart';
 import 'package:eastarrow_app/presentation/my/my_page_model.dart';
-import 'package:eastarrow_app/theme/app_colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,26 +11,133 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => MyPageModel(),
-      child: Consumer<MyPageModel>(builder: (context, model, snapshot) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('マイページ'),
-          ),
-          endDrawer: Drawer(),
-          body: Column(
-            children: [
-              const SizedBox(height: 8),
-              TextField(
-                autofocus: false,
-                controller: model.nameController,
-                onChanged: (text) {
-                  print(model.nameController);
-                },
+      child: Consumer<MyPageModel>(
+        builder: (context, model, child) {
+          return Scaffold(
+              appBar: AppBar(
+                title: const Text('マイページ'),
               ),
-            ],
-          ),
-        );
-      }),
+              endDrawer: const Drawer(),
+              body: GestureDetector(
+                onTap: () {
+                  FocusScopeNode currentFocus = FocusScope.of(context);
+                  if (!currentFocus.hasPrimaryFocus) {
+                    currentFocus.unfocus();
+                  }
+                },
+                child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        '名前',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextField(
+                        autofocus: false,
+                        controller: model.nameController,
+                        keyboardType: TextInputType.text,
+                        textAlign: TextAlign.end,
+                        onChanged: (text) => model.nameController.text = text,
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        'メールアドレス',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextField(
+                        autofocus: false,
+                        controller: model.mailController,
+                        keyboardType: TextInputType.emailAddress,
+                        textAlign: TextAlign.end,
+                        onChanged: (text) {},
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        '生年月日',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextField(
+                        autofocus: false,
+                        controller: model.birthdayController,
+                        textAlign: TextAlign.end,
+                        onChanged: (text) {},
+                        onTap: () async => await model.selectBirthday(context),
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        '住所',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextField(
+                        autofocus: false,
+                        controller: model.addressController,
+                        keyboardType: TextInputType.streetAddress,
+                        textAlign: TextAlign.end,
+                        onChanged: (text) {},
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        '電話番号',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextField(
+                        autofocus: false,
+                        controller: model.phoneNumberController,
+                        keyboardType: TextInputType.phone,
+                        textAlign: TextAlign.end,
+                        onChanged: (text) {},
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        '車種',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextField(
+                        autofocus: false,
+                        controller: model.vehicleTypeController,
+                        keyboardType: TextInputType.text,
+                        textAlign: TextAlign.end,
+                        onChanged: (text) {},
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        '車検日',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextField(
+                        autofocus: false,
+                        controller: model.inspectionController,
+                        textAlign: TextAlign.end,
+                        onChanged: (text) {},
+                        onTap: () async =>
+                            await model.selectInspectionDay(context),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(20.0),
+                        child: SizedBox(
+                          height: 50,
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () =>
+                                showConfirmDialog(context, 'ユーザー情報を更新しますか？'),
+                            child: const Text('ユーザー情報を更新'),
+                          ), //TODO 情報変更時ボタンをアクティブに＆DBをupdate
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
