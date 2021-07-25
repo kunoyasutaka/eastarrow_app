7a669ab5eef965a95de22756961354593d41cc49
kunoyasutaka <81798624+kunoyasutaka@users.noreply.github.com>
2021-07-25 23:51:00 +0900

    不要なメアド削除

---
+++ lib/domain/chat.dart
@@ -0,0 +1,15 @@
+class Chat {
+  final String sender;
+  final String title;
+  final String date;
+  final String description;
+  final String imageURL;
+
+  Chat(
+    this.sender,
+    this.title,
+    this.date,
+    this.description,
+    this.imageURL,
+  );
+}
