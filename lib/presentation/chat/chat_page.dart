a8b0f88b77e9adf79b7bdb6a9548e72325f26d82
sekijunichi <sekisan_kgr@icloud.com>
2021-07-27 16:15:25 +0900

    7/27 pull request コメント内容修正

--- lib/presentation/chat/chat_page.dart
+++ lib/presentation/chat/chat_page.dart
@@ -13,26 +13,119 @@ class ChatPage extends StatelessWidget {
       child: Consumer<ChatModel>(
         builder: (context, model, child) {
           return Scaffold(
-              appBar: AppBar(
-                title: const Text('連絡'),
-                centerTitle: true,
-              ),
-              endDrawer: const Drawer(),
-              body: GestureDetector(
-                onTap: () {
-                  FocusScopeNode currentFocus = FocusScope.of(context);
-                  if (!currentFocus.hasPrimaryFocus) {
-                    currentFocus.unfocus();
-                  }
-                },
-                child: Column(
+            appBar: AppBar(
+              title: const Text('連絡'),
+              centerTitle: true,
+            ),
+            endDrawer: const Drawer(),
+            body: GestureDetector(
+              onTap: () {
+                FocusScopeNode currentFocus = FocusScope.of(context);
+                if (!currentFocus.hasPrimaryFocus) {
+                  currentFocus.unfocus();
+                }
+              },
+              child: Column(
                 children: [
                   Expanded(
                     child: ListView.builder(
                       itemCount: model.chatLog.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return chatLogTile(
-                            context, model.chatLog[index], model);
+                      itemBuilder: (BuildContext context, int logIndex) {
+                        return Container(
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(color: Colors.grey, width: 1),
+                            ),
+                          ),
+                          child: ExpansionTile(
+                            trailing: IconButton(
+                                onPressed: () => model.subjectController.text =
+                                    model.chatLog[logIndex].title,
+                                icon: const Icon(Icons.reply)),
+                            title: Text('件名：${model.chatLog[logIndex].title}'),
+                            children: [
+                              ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  itemCount:
+                                      model.chatLog[logIndex].chatDetail.length,
+                                  itemBuilder:
+                                      (BuildContext context, int detailIndex) {
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                        border: Border(
+                                          top: BorderSide(
+                                              color: Colors.grey.shade400,
+                                              width: 1),
+                                        ),
+                                      ),
+                                      alignment: Alignment.centerLeft,
+                                      padding: const EdgeInsets.only(
+                                          left: 40,
+                                          top: 12,
+                                          right: 20,
+                                          bottom: 12),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              model
+                                                          .chatLog[logIndex]
+                                                          .chatDetail[
+                                                              detailIndex]
+                                                          .sender ==
+                                                      '管理者'
+                                                  ? const Icon(Icons.mail,
+                                                      size: 20)
+                                                  : const Icon(Icons.person,
+                                                      size: 20),
+                                              const SizedBox(width: 12),
+                                              Text(model
+                                                  .chatLog[logIndex]
+                                                  .chatDetail[detailIndex]
+                                                  .sender),
+                                              const Expanded(child: SizedBox()),
+                                              Text(model
+                                                  .chatLog[logIndex]
+                                                  .chatDetail[detailIndex]
+                                                  .date),
+                                            ],
+                                          ),
+                                          Container(
+                                            padding: const EdgeInsets.all(12),
+                                            child: Text(model
+                                                .chatLog[logIndex]
+                                                .chatDetail[detailIndex]
+                                                .description),
+                                          ),
+                                          model
+                                                      .chatLog[logIndex]
+                                                      .chatDetail[detailIndex]
+                                                      .imageURL !=
+                                                  ''
+                                              ? Container(
+                                                  padding:
+                                                      const EdgeInsets.all(12),
+                                                  child: Image.network(
+                                                    model
+                                                        .chatLog[logIndex]
+                                                        .chatDetail[detailIndex]
+                                                        .imageURL,
+                                                    width: double.infinity,
+                                                  ),
+                                                )
+                                              : Container(),
+                                        ],
+                                      ),
+                                    );
+                                  }
+                                  // chatDetailTile(context, model.chatLog[index1].chatDetail[index2]),
+                                  ),
+                            ],
+                          ),
+                        );
                       },
                     ),
                   ),
@@ -94,66 +187,6 @@ class ChatPage extends StatelessWidget {
             ),
           );
         },
-      ),
-    );
-  }
-
-  Widget chatLogTile(BuildContext context, ChatLog chatLog, ChatModel model) {
-    return Container(
-      decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(color: Colors.grey, width: 1),
-        ),
-      ),
-      child: ExpansionTile(
-        trailing: IconButton(
-            onPressed: () => model.subjectController.text = chatLog.title,
-            icon: const Icon(Icons.reply)),
-        title: Text('件名：${chatLog.title}'),
-        children: chatLog.chatDetail
-            .map((ChatDetail chatDetail) => chatDetailTile(context, chatDetail))
-            .toList(),
-      ),
-    );
-  }
-
-  Widget chatDetailTile(BuildContext context, ChatDetail chatDetail) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(color: Colors.grey.shade400, width: 1),
-        ),
-      ),
-      alignment: Alignment.centerLeft,
-      padding: const EdgeInsets.only(left: 40, top: 12, right: 20, bottom: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              chatDetail.sender == '管理者'
-                  ? const Icon(Icons.mail, size: 20)
-                  : const Icon(Icons.person, size: 20),
-              const SizedBox(width: 12),
-              Text(chatDetail.sender),
-              const Expanded(child: SizedBox()),
-              Text(chatDetail.date),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.all(12),
-            child: Text(chatDetail.description),
-          ),
-          chatDetail.imageURL != ''
-              ? Container(
-                  padding: const EdgeInsets.all(12),
-                  child: Image.network(
-                    chatDetail.imageURL,
-                    width: double.infinity,
-                  ),
-                )
-              : Container(),
-        ],
       ),
     );
   }
