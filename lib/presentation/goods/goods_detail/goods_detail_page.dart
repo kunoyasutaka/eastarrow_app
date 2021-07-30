a1c1bdb4cf26e5302b0e0fc5db2bce52d832eabe
sekijunichi <sekisan_kgr@icloud.com>
2021-07-30 23:27:37 +0900

    7/30 入荷ページのレイアウトを編集・CarouselSliderを採用

--- lib/presentation/goods/goods_detail/goods_detail_page.dart
+++ lib/presentation/goods/goods_detail/goods_detail_page.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:eastarrow_app/presentation/common/dialog.dart';
 import 'package:eastarrow_app/presentation/goods/goods_detail/goods_detail_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -37,174 +39,251 @@ class GoodsDetailPage extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      width: double.infinity,
+                      decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey, width: 1))),
+                      child: Text(
+                        name!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12, top: 12),
+                      child: Text(
+                        introduction!,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 32),
                     Padding(
                       padding: const EdgeInsets.only(bottom: 8.0),
-                      child: Image.network(url!),
-                    ),
-                    Text(
-                      name!,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
+                      child: CarouselSlider.builder(
+                        itemCount: model.detailImgList.length,
+                        itemBuilder: (BuildContext context, int index, int index2) {
+                          return Stack(
+                            alignment: Alignment.topRight,
+                            children: [
+                              Image.network(model.detailImgList[index]),
+                              Container(
+                                child: Text(
+                                  '${index + 1}/${model.detailImgList.length}',
+                                  style: const TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                width: 40,
+                                height: 28,
+                                color: Colors.white,
+                                alignment: Alignment.center,
+                              ),
+                            ],
+                          );
+                        },
+                        carouselController: model.carouselController,
+                        options: CarouselOptions(
+                          autoPlay: true,
+                          enlargeCenterPage: true,
+                          viewportFraction: 0.7,
+                        ),
                       ),
                     ),
-                    Text(
-                      introduction!,
-                      style: const TextStyle(
-                        fontSize: 16,
+                    const SizedBox(height: 32),
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      width: double.infinity,
+                      decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey, width: 1))),
+                      child: const Text(
+                        '商品の情報',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    const Text('商品の情報'),
-                    const Divider(color: Colors.grey),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          model.bodyValueString,
-                          style: const TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text(
-                              model.bodyValue,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                model.bodyValueString,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
                               ),
-                            ),
-                            const Text(
-                              '万円',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    model.bodyValue,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const Text(
+                                    '万円',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    const Divider(color: Colors.grey),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          model.totalValueString,
-                          style: const TextStyle(
-                            fontSize: 20,
+                            ],
                           ),
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text(
-                              model.totalValue,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+                          const Divider(color: Colors.grey),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                model.totalValueString,
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
                               ),
-                            ),
-                            const Text(
-                              '万円',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    model.totalValue,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const Text(
+                                    '万円',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    const Divider(color: Colors.grey),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          '年式',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        Text(
-                          model.year,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                    const Divider(color: Colors.grey),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          '走行距離',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        Text(
-                          model.mileage,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                          const Divider(color: Colors.grey),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '年式',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                model.year,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                    const Divider(color: Colors.grey),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          '車検有無',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        Text(
-                          model.checkYear,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                          const Divider(color: Colors.grey),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '走行距離',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                model.mileage,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                    const Divider(color: Colors.grey),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          '修理歴',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        Text(
-                          model.repair,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                          const Divider(color: Colors.grey),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '車検有無',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                model.checkYear,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                    const Divider(color: Colors.grey),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          '地域',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        Text(
-                          model.location,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                          const Divider(color: Colors.grey),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '修理歴',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                model.repair,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
+                          const Divider(color: Colors.grey),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text(
+                                '地域',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                model.location,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Divider(color: Colors.grey),
+                          const SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () => showConfirmDialog(context, '在庫確認を依頼します。\n後ほど「連絡画面」にメッセージをお送りいたします。'),
+                                child: Container(
+                                  padding: const EdgeInsets.all(12),
+                                  child: const Text(
+                                    '在庫確認',
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () => showConfirmDialog(context, '見積依頼をします。\n後ほど「連絡画面」にメッセージをお送りいたします。'),
+                                child: Container(
+                                  padding: const EdgeInsets.all(12),
+                                  child: const Text(
+                                    '見積依頼',
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                    const Divider(color: Colors.grey),
-                    const SizedBox(height: 40),
                   ],
                 ),
               ),
