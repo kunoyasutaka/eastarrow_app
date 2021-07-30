f63448104fff738589fe218beca1754d5425f602
sekijun1 <70083776+sekijun1@users.noreply.github.com>
2021-07-30 22:05:23 +0900

    #9 緊急連絡ページの作成 (#10)
    * 7/29 緊急連絡ページの作成
    
    * 7/29 pull request コメント対応（controller, 画面レイアウト）
    
    * 7/29 pull request コメント対応（SingleChildScrollViewでラップ）

---
+++ lib/presentation/admission/emergency_detail/emergency_repair_page.dart
@@ -0,0 +1,122 @@
+import 'package:eastarrow_app/presentation/admission/emergency_model.dart';
+import 'package:eastarrow_app/presentation/common/dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class EmergencyRepairPage extends StatelessWidget {
+  const EmergencyRepairPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => EmergencyModel(),
+      child: Consumer<EmergencyModel>(
+        builder: (context, model, child) {
+          return Column(
+            children: [
+              Container(
+                height: 60,
+                width: double.infinity,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: 1, color: Colors.grey),
+                  ),
+                ),
+                child: const Text(
+                  '修理・整備についての詳細入力',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'メーカー',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          autofocus: false,
+                          controller: model.carMakerController,
+                          keyboardType: TextInputType.text,
+                          onChanged: (text) {},
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          '車種',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          autofocus: false,
+                          controller: model.carTypeController,
+                          keyboardType: TextInputType.text,
+                          onChanged: (text) {},
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          '車検有効期限',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          autofocus: false,
+                          controller: model.inspectionExpirationController,
+                          keyboardType: TextInputType.text,
+                          onChanged: (text) {},
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'お問い合わせ詳細',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          autofocus: false,
+                          controller: model.inquiryController,
+                          maxLines: 2,
+                          decoration: const InputDecoration(border: OutlineInputBorder()),
+                          keyboardType: TextInputType.text,
+                          onChanged: (text) {},
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          '備考',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        TextField(
+                          autofocus: false,
+                          controller: model.remarkController,
+                          maxLines: 2,
+                          decoration: const InputDecoration(border: OutlineInputBorder()),
+                          keyboardType: TextInputType.text,
+                          onChanged: (text) {},
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          padding: const EdgeInsets.all(20.0),
+                          child: SizedBox(
+                            height: 50,
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                await showConfirmDialog(context, 'ご記入いただいた内容を送信します。\nよろしいですか？');
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('こちらの内容で送信'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/presentation/admission/emergency_model.dart
+++ lib/presentation/admission/emergency_model.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-class EmergencyModel extends ChangeNotifier {}
+class EmergencyModel extends ChangeNotifier {
+  final policyNumberController = TextEditingController();
+  final carDamageController = TextEditingController();
+  final injuryController = TextEditingController();
+  final opponentController = TextEditingController();
+  final accidentSituationController = TextEditingController();
+
+  final carMakerController = TextEditingController();
+  final carTypeController = TextEditingController();
+  final inspectionExpirationController = TextEditingController();
+  final inquiryController = TextEditingController();
+  final remarkController = TextEditingController();
+
+
+}
