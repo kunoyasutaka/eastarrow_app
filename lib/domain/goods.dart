51db552ea93f303559c84a87a0f23de89ec8493d
kunoyasutaka <81798624+kunoyasutaka@users.noreply.github.com>
2021-08-01 22:48:44 +0900

    goodsモデル追加

---
+++ lib/domain/goods.dart
@@ -0,0 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class GoodsField {
+  static const id = 'id';
+  static const name = 'name';
+  static const introduction = 'introduction';
+  static const bodyValue = 'bodyValue';
+  static const totalValue = 'totalValue';
+  static const modelYear = 'modelYear';
+  static const mileage = 'mileage';
+  static const inspection = 'inspection';
+  static const repair = 'repair';
+  static const area = 'area';
+  static const createdAt = 'createdAt';
+  static const updatedAt = 'updatedAt';
+}
+
+class Goods {
+  final String? id;
+  final String? name;
+  final String? introduction;
+  final String? bodyValue;
+  final String? totalValue;
+  final String? modelYear;
+  final String? mileage;
+  final String? inspection;
+  final String? repair;
+  final String? area;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  Goods({
+    this.id,
+    this.name,
+    this.introduction,
+    this.bodyValue,
+    this.totalValue,
+    this.modelYear,
+    this.mileage,
+    this.inspection,
+    this.repair,
+    this.area,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory Goods.fromFirestore(DocumentSnapshot snap) {
+    final Map<String, dynamic> data = snap.data() as Map<String, dynamic>;
+
+    return Goods(
+      id: data[GoodsField.id] ?? '',
+      name: data[GoodsField.name] ?? '',
+      introduction: data[GoodsField.introduction] ?? '',
+      bodyValue: data[GoodsField.bodyValue] ?? '',
+      totalValue: data[GoodsField.totalValue] ?? '',
+      modelYear: data[GoodsField.modelYear] ?? '',
+      mileage: data[GoodsField.mileage] ?? '',
+      inspection: data[GoodsField.inspection] ?? '',
+      repair: data[GoodsField.repair] ?? '',
+      area: data[GoodsField.area] ?? '',
+      createdAt: data[GoodsField.createdAt],
+      updatedAt: data[GoodsField.updatedAt],
+    );
+  }
+}
