b0f2cffa2fdf5fdeb5adaa5efd78783fb61eceab
sekijun1 <70083776+sekijun1@users.noreply.github.com>
2021-08-01 14:06:11 +0900

    #11 入荷ページのレイアウトを編集（仮）・CarouselSliderを採用 (#12)
    * 7/30 入荷ページのレイアウトを編集・CarouselSliderを採用
    
    * 8/1 pull request訂正（画像拡大・ボタン削除・画像内文字色訂正）

--- lib/presentation/goods/goods_list/goods_list_page.dart
+++ lib/presentation/goods/goods_list/goods_list_page.dart
@@ -17,13 +17,12 @@ class GoodsListPage extends StatelessWidget {
               title: const Text('入荷情報'),
               centerTitle: true,
             ),
-            endDrawer: Drawer(),
+            endDrawer: const Drawer(),
             body: ListView.builder(
               itemCount: model.imgList.length,
               itemBuilder: (BuildContext context, int index) {
                 return Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15)),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: InkWell(
@@ -31,17 +30,38 @@ class GoodsListPage extends StatelessWidget {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => GoodsDetailPage(
-                              url: model.imgList[index],
-                              name: model.goodsName,
-                              introduction: model.introduction,
-                            ),
-                          ),
+                              builder: (context) => GoodsDetailPage(
+                                    url: model.imgList[index],
+                                    name: model.goodsName,
+                                    introduction: model.introduction,
+                                  ),
+                              fullscreenDialog: true),
                         );
                       },
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          Container(
+                            padding: const EdgeInsets.only(bottom: 8),
+                            width: double.infinity,
+                            decoration:
+                                const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey, width: 1))),
+                            child: Text(
+                              model.goodsName,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 12, top: 12),
+                            child: Text(
+                              model.introduction,
+                              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
                           Stack(
                             alignment: Alignment.bottomRight,
                             children: [
@@ -58,19 +78,6 @@ class GoodsListPage extends StatelessWidget {
                                 ),
                               ),
                             ],
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            model.goodsName,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            model.introduction,
-                            style: const TextStyle(fontSize: 12),
                           ),
                           const SizedBox(height: 8),
                         ],
