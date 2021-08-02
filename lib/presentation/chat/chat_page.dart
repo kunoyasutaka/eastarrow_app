02b4f904628ee4ef7d144bc33e363a61859bc6d5
kunoyasutaka <81798624+kunoyasutaka@users.noreply.github.com>
2021-08-02 20:09:32 +0900

    インディケーターを追加

--- lib/presentation/chat/chat_page.dart
+++ lib/presentation/chat/chat_page.dart
@@ -25,135 +25,168 @@ class ChatPage extends StatelessWidget {
                   currentFocus.unfocus();
                 }
               },
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: model.chatLog.length,
-                      itemBuilder: (BuildContext context, int logIndex) {
-                        return Container(
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(color: Colors.grey, width: 1),
-                            ),
-                          ),
-                          child: ExpansionTile(
-                            trailing: IconButton(
-                                onPressed: () => model.subjectController.text =
-                                    model.chatLog[logIndex].title,
-                                icon: const Icon(Icons.reply)),
-                            title: Text('件名：${model.chatLog[logIndex].title}'),
-                            children: [
-                              ListView.builder(
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                itemCount: model.chatLog[logIndex].chatDetail.length,
-                                itemBuilder: (BuildContext context, int detailIndex) {
-                                  return Container(
-                                    decoration: BoxDecoration(
-                                      border: Border(
-                                        top: BorderSide(
-                                            color: Colors.grey.shade400,
-                                            width: 1),
-                                      ),
-                                    ),
-                                    alignment: Alignment.centerLeft,
-                                    padding: const EdgeInsets.only(
-                                        left: 40,
-                                        top: 12,
-                                        right: 20,
-                                        bottom: 12),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          children: [model.chatLog[logIndex].chatDetail[detailIndex].sender == '管理者'
-                                                ? const Icon(Icons.mail, size: 20)
-                                                : const Icon(Icons.person, size: 20),
-                                            const SizedBox(width: 12),
-                                            Text(model.chatLog[logIndex].chatDetail[detailIndex].sender),
-                                            const Expanded(child: SizedBox()),
-                                            Text(model.chatLog[logIndex].chatDetail[detailIndex].date),
-                                          ],
-                                        ),
-                                        Container(
-                                          padding: const EdgeInsets.all(12),
-                                          child: Text(model.chatLog[logIndex].chatDetail[detailIndex].description),
-                                        ),
-                                        model.chatLog[logIndex].chatDetail[detailIndex].imageURL !=''
-                                            ? Container(
-                                                padding: const EdgeInsets.all(12),
-                                                child: Image.network(
-                                                  model.chatLog[logIndex].chatDetail[detailIndex].imageURL,
-                                                  width: double.infinity,
-                                                ),
-                                              )
-                                            : Container(),
-                                      ],
-                                    ),
-                                  );
-                                },
+              child: RefreshIndicator(
+                onRefresh: () async => await model.fetchChatData(),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: model.chatLog.length,
+                        itemBuilder: (BuildContext context, int logIndex) {
+                          return Container(
+                            decoration: const BoxDecoration(
+                              border: Border(
+                                bottom:
+                                    BorderSide(color: Colors.grey, width: 1),
                               ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(12),
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        top: BorderSide(color: Colors.grey),
+                            ),
+                            child: ExpansionTile(
+                              trailing: IconButton(
+                                  onPressed: () => model.subjectController
+                                      .text = model.chatLog[logIndex].title,
+                                  icon: const Icon(Icons.reply)),
+                              title:
+                                  Text('件名：${model.chatLog[logIndex].title}'),
+                              children: [
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  itemCount:
+                                      model.chatLog[logIndex].chatDetail.length,
+                                  itemBuilder:
+                                      (BuildContext context, int detailIndex) {
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                        border: Border(
+                                          top: BorderSide(
+                                              color: Colors.grey.shade400,
+                                              width: 1),
+                                        ),
+                                      ),
+                                      alignment: Alignment.centerLeft,
+                                      padding: const EdgeInsets.only(
+                                          left: 40,
+                                          top: 12,
+                                          right: 20,
+                                          bottom: 12),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              model
+                                                          .chatLog[logIndex]
+                                                          .chatDetail[
+                                                              detailIndex]
+                                                          .sender ==
+                                                      '管理者'
+                                                  ? const Icon(Icons.mail,
+                                                      size: 20)
+                                                  : const Icon(Icons.person,
+                                                      size: 20),
+                                              const SizedBox(width: 12),
+                                              Text(model
+                                                  .chatLog[logIndex]
+                                                  .chatDetail[detailIndex]
+                                                  .sender),
+                                              const Expanded(child: SizedBox()),
+                                              Text(model
+                                                  .chatLog[logIndex]
+                                                  .chatDetail[detailIndex]
+                                                  .date),
+                                            ],
+                                          ),
+                                          Container(
+                                            padding: const EdgeInsets.all(12),
+                                            child: Text(model
+                                                .chatLog[logIndex]
+                                                .chatDetail[detailIndex]
+                                                .description),
+                                          ),
+                                          model
+                                                      .chatLog[logIndex]
+                                                      .chatDetail[detailIndex]
+                                                      .imageURL !=
+                                                  ''
+                                              ? Container(
+                                                  padding:
+                                                      const EdgeInsets.all(12),
+                                                  child: Image.network(
+                                                    model
+                                                        .chatLog[logIndex]
+                                                        .chatDetail[detailIndex]
+                                                        .imageURL,
+                                                    width: double.infinity,
+                                                  ),
+                                                )
+                                              : Container(),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
                       ),
                     ),
-                    child: Column(
-                      children: [
-                        Row(
-                          children: [
-                            Flexible(
-                              child: TextField(
-                                decoration:
-                                    const InputDecoration(hintText: '件名'),
-                                keyboardType: TextInputType.text,
-                                controller: model.subjectController,
+                    Container(
+                      padding: const EdgeInsets.all(12),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Flexible(
+                                child: TextField(
+                                  decoration:
+                                      const InputDecoration(hintText: '件名'),
+                                  keyboardType: TextInputType.text,
+                                  controller: model.subjectController,
+                                ),
                               ),
-                            ),
-                            IconButton(
-                              onPressed: () async =>
-                                  await showTextDialog(context, '画像を選択してください。'),
-                              icon: const Icon(Icons.image),
-                            ),
-                            //TODO ImagePickerでファイルを取得
-                            IconButton(
-                              onPressed: () async =>
-                                  await showTextDialog(context, '写真を撮影してください。'),
-                              icon: const Icon(Icons.camera_alt),
-                            ),
-                            //TODO カメラを開く
-                            IconButton(
-                              onPressed: () async =>
-                                  await showConfirmDialog(context, '送信しますか？'),
-                              icon: const Icon(Icons.send),
-                            ), //TODO 送信処理
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        TextField(
-                          decoration: const InputDecoration(
-                            hintText: '本文',
-                            border: OutlineInputBorder(),
+                              IconButton(
+                                onPressed: () async => await showTextDialog(
+                                    context, '画像を選択してください。'),
+                                icon: const Icon(Icons.image),
+                              ),
+                              //TODO ImagePickerでファイルを取得
+                              IconButton(
+                                onPressed: () async => await showTextDialog(
+                                    context, '写真を撮影してください。'),
+                                icon: const Icon(Icons.camera_alt),
+                              ),
+                              //TODO カメラを開く
+                              IconButton(
+                                onPressed: () async =>
+                                    await showConfirmDialog(context, '送信しますか？'),
+                                icon: const Icon(Icons.send),
+                              ), //TODO 送信処理
+                            ],
                           ),
-                          keyboardType: TextInputType.multiline,
-                          maxLines: 3,
-                          controller: model.mainTextController,
-                        ),
-                      ],
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          TextField(
+                            decoration: const InputDecoration(
+                              hintText: '本文',
+                              border: OutlineInputBorder(),
+                            ),
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 3,
+                            controller: model.mainTextController,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           );
