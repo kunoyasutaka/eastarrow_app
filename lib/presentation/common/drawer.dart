18f7f6fb21595566f1f558026afbdf0fd21c4a0f
kunoyasutaka <81798624+kunoyasutaka@users.noreply.github.com>
2021-08-04 00:45:11 +0900

    色設定変更・ログアウト追加

--- lib/presentation/common/drawer.dart
+++ lib/presentation/common/drawer.dart
@@ -1,39 +1,53 @@
+import 'package:eastarrow_app/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 
 Widget drawer() {
   return Drawer(
-    child: ListView(
-      children: [
-        Container(
-          child: const Text(
-            'MENU',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    child: Container(
+      color: AppColors.lightGrey,
+      child: ListView(
+        children: [
+          Container(
+            child: const Text(
+              'MENU',
+              style: TextStyle(
+                  color: AppColors.navyBlue,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+            padding: const EdgeInsets.only(left: 24, top: 16, bottom: 16),
+            margin: EdgeInsets.zero,
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(width: 1, color: Colors.grey))),
           ),
-          padding: const EdgeInsets.only(left: 24, top: 16, bottom: 16),
-          margin: EdgeInsets.zero,
-          decoration: const BoxDecoration(border: Border(bottom: BorderSide(width: 1, color: Colors.grey))),
-        ),
-        const ListTile(
-          leading: Icon(Icons.person, size: 28),
-          title: Text('会員情報を編集', style: TextStyle(fontSize: 16)),
-        ),
-        const Divider(),
-        const ListTile(
-          leading: Icon(Icons.corporate_fare, size: 28),
-          title: Text('HPはこちら', style: TextStyle(fontSize: 16)),
-        ),
-        const Divider(),
-        const ListTile(
-          leading: Icon(Icons.account_circle, size: 28),
-          title: Text('プライバシーポリシー', style: TextStyle(fontSize: 16)),
-        ),
-        const Divider(),
-        const ListTile(
-          leading: Icon(Icons.assignment_outlined, size: 28),
-          title: Text('利用規約', style: TextStyle(fontSize: 16)),
-        ),
-        const Divider(),
-      ],
+          const ListTile(
+            leading: Icon(Icons.person, size: 28),
+            title: Text('会員情報を編集', style: TextStyle(fontSize: 16)),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(Icons.corporate_fare, size: 28),
+            title: Text('HPはこちら', style: TextStyle(fontSize: 16)),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(Icons.account_circle, size: 28),
+            title: Text('プライバシーポリシー', style: TextStyle(fontSize: 16)),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(Icons.assignment_outlined, size: 28),
+            title: Text('利用規約', style: TextStyle(fontSize: 16)),
+          ),
+          const Divider(),
+          const ListTile(
+            leading: Icon(Icons.logout, size: 28),
+            title: Text('ログアウト', style: TextStyle(fontSize: 16)),
+          ),
+          const Divider(),
+        ],
+      ),
     ),
   );
 }
