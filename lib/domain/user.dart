1058b601c8e29b13b367433bca6081baf433eb17
sekijunichi <sekisan_kgr@icloud.com>
2021-08-10 14:09:42 +0900

    ChatTitleFieldを削除

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -62,12 +62,6 @@ class User {
   }
 }
 
-class ChatTitleField {
-  static const docId = 'docId';
-  static const title = 'title';
-  static const createdAt = 'createdAt';
-}
-
 class ChatTitle {
   final String? docId;
   final String? title;
