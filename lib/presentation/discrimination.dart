0948c53bce0dccd726622c15654e28befb8af36b
kunoyasutaka <81798624+kunoyasutaka@users.noreply.github.com>
2021-08-20 17:26:11 +0900

    ログイン済みを判定する処理を仮で追加

--- lib/presentation/discrimination.dart
+++ lib/presentation/discrimination.dart
@@ -1,16 +1,16 @@
-// import 'package:eastarrow_app/presentation/login/login_page.dart';
-// import 'package:eastarrow_app/presentation/root.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
+import 'package:eastarrow_app/presentation/login/login_page.dart';
+import 'package:eastarrow_app/presentation/root.dart';
+import 'package:eastarrow_app/repository/auth_repository.dart';
+import 'package:flutter/material.dart';
 
-// class DiscriminationPage extends StatelessWidget {
-//   const DiscriminationPage({Key? key}) : super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//     if (AuthRepository().isSignIn) {
-//       return const Root();
-//     } else {
-//       return const LoginPage();
-//     }
-//   }
-// }
+class DiscriminationPage extends StatelessWidget {
+  const DiscriminationPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    if (AuthRepository().isSignedIn) {
+      return const Root();
+    } else {
+      return const LoginPage();
+    }
+  }
+}
