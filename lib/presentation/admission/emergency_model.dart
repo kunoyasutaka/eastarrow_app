e363ea11a8e426ebbdd233397bedfc58b8c27ee5
sekijunichi <sekisan_kgr@icloud.com>
2021-09-21 13:55:55 +0900

    url_launcherをインストールして電話をかけられる機能を追加

--- lib/presentation/admission/emergency_model.dart
+++ lib/presentation/admission/emergency_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EmergencyModel extends ChangeNotifier {
   ///必要であれば実装
@@ -9,4 +10,13 @@ class EmergencyModel extends ChangeNotifier {
   //   member = await repository.fetchMember(FirebaseAuth.instance.currentUser!.uid);
   //   notifyListeners();
   // }
+  Future<void> phoneCall() async {
+    const phoneURL = "tel:+819018621477";
+    if (await canLaunch(phoneURL)) {
+      launch(phoneURL);
+    } else {
+      final Error error = ArgumentError('Could not launch $phoneURL');
+      throw error;
+    }
+  }
 }
--- lib/presentation/admission/emergency_page.dart
+++ lib/presentation/admission/emergency_page.dart
@@ -31,8 +31,7 @@ class EmergencyPage extends StatelessWidget {
                       width: double.infinity,
                       child: const Text(
                         '緊急連絡',
-                        style: TextStyle(
-                            fontSize: 28, fontWeight: FontWeight.bold),
+                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                       ),
                       decoration: const BoxDecoration(
                         border: Border(
@@ -54,8 +53,7 @@ class EmergencyPage extends StatelessWidget {
                           ),
                           const Text(
                             '・事故・保険のお問い合わせ',
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                           ),
                           Container(
                             padding: const EdgeInsets.all(12),
@@ -72,9 +70,7 @@ class EmergencyPage extends StatelessWidget {
                                   ),
                                   builder: (BuildContext context) {
                                     return SizedBox(
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.8,
+                                      height: MediaQuery.of(context).size.height * 0.8,
                                       child: const EmergencyAccidentPage(),
                                     );
                                   },
@@ -90,8 +86,7 @@ class EmergencyPage extends StatelessWidget {
                           const SizedBox(height: 16),
                           const Text(
                             '・修理・整備のお問い合わせ',
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                           ),
                           Container(
                             padding: const EdgeInsets.all(12),
@@ -108,9 +103,7 @@ class EmergencyPage extends StatelessWidget {
                                   ),
                                   builder: (BuildContext context) {
                                     return SizedBox(
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.8,
+                                      height: MediaQuery.of(context).size.height * 0.8,
                                       child: const EmergencyRepairPage(),
                                     );
                                   },
@@ -133,8 +126,7 @@ class EmergencyPage extends StatelessWidget {
                       width: double.infinity,
                       child: const Text(
                         '緊急通話',
-                        style: TextStyle(
-                            fontSize: 28, fontWeight: FontWeight.bold),
+                        style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                       ),
                       decoration: const BoxDecoration(
                         border: Border(
@@ -155,8 +147,11 @@ class EmergencyPage extends StatelessWidget {
                             padding: const EdgeInsets.all(12),
                             width: double.infinity,
                             child: ElevatedButton.icon(
-                              onPressed: () => showConfirmDialog(
-                                  context, '店舗に電話します。\nよろしいですか。'),
+                              onPressed: () async {
+                                if (await showConfirmDialog(context, '店舗に電話します。\nよろしいですか。')) {
+                                  model.phoneCall();
+                                }
+                              },
                               icon: const Icon(Icons.phone),
                               label: const Text(
                                 '店舗に電話する',
